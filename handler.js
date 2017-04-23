cc0340d3b871f42dc435af9112326dd261f1f6da
kaylerch <dekstar@gmx.de>
2017-04-23 22:32:40 +0200

    Change file structure

--- handler.js
+++ handler.js
@@ -15,7 +15,7 @@ module.exports.feed = (event, context, callback) => {
 
     var params = {
       Bucket: 'io.klerch.alexa.once',
-      Key: resort + '/' + dateFormat(upDate, 'mm-dd') + '.json'
+      Key: dateFormat(upDate, 'mm') + '/' + dateFormat(upDate, 'dd') + '/' + resort + '-' + dateFormat(upDate, 'mm-dd') + '.json'
     };
     s3.getObject(params, function(err, data) {
         var feeds = [];
@@ -26,26 +26,30 @@ module.exports.feed = (event, context, callback) => {
           var body = JSON.parse(data.Body);
           var years = Object.keys(body);
           var ms = 0;
-          var exactly = ['exakt','genau',''].sort( function() { return 0.5 - Math.random() } );
-          var happened = ['Geschehen','Passiert','Ereignet'].sort( function() { return 0.5 - Math.random() } );
+          var exactly = ['exakt','genau',''].sort(function() { return 0.5 - Math.random() } );
+          var happened = ['Geschehen','Passiert','Ereignet'].sort(function() { return 0.5 - Math.random() } );
           // shuffle list and pick last three
-          years.sort( function() { return 0.5 - Math.random() } ).slice(-3).sort().reverse().forEach(function (year) {
-            var weekday = new Date(year, upDate.getMonth(), upDate.getDate()).getDay();
-            var dateStr = weekdays[weekday] + ' den ' + dateFormat(upDate, 'dd.') + months[upDate.getMonth()] + ' ' + year.replace('-', '') + (year.startsWith('-') ? ' vor Christus' : '');
-            var text = (resort == 'birthdays' ?
-                ("Geboren vor " + (thisYear - parseInt(year)) + " Jahren wurde " + body[year].text + ", am " + dateStr) :
-                (happened[ms] + " vor " + exactly[ms] + " " + (thisYear - parseInt(year)) + " Jahren, am " + dateStr + ". " + body[year].text));
-            // ensure unique update-timestamp
-            upDate.setMilliseconds(ms++);
-            var feed = {
-                uid: "urn:uuid:" + uuidV4(),
-                updateDate: upDate.toISOString(),
-                titleText: dateStr + ": " + body[year].title,
-                mainText: text,
-                redirectionUrl: body[year].link
-            };
-            feeds.push(feed);
-          });
+          years.sort(function() { return 0.5 - Math.random() } ) // shuffle
+               .slice(-3) // pick last three
+               .sort(function(a,b) { return a - b } ) // sort by year 
+               .reverse() // most recent events come first
+               .forEach(function (year) {
+                  var weekday = new Date(year, upDate.getMonth(), upDate.getDate()).getDay();
+                  var dateStr = weekdays[weekday] + ' den ' + dateFormat(upDate, 'dd.') + months[upDate.getMonth()] + ' ' + year.replace('-', '') + (year.startsWith('-') ? ' vor Christus' : '');
+                  var text = (resort == 'birthdays' ?
+                      ("Geboren vor " + (thisYear - parseInt(year)) + " Jahren wurde " + body[year].text + ", am " + dateStr) :
+                      (happened[ms] + " vor " + exactly[ms] + " " + (thisYear - parseInt(year)) + " Jahren, am " + dateStr + ". " + body[year].text));
+                  // ensure unique update-timestamp
+                  upDate.setMilliseconds(ms++);
+                  var feed = {
+                      uid: "urn:uuid:" + uuidV4(),
+                      updateDate: upDate.toISOString(),
+                      titleText: dateStr + ": " + body[year].title,
+                      mainText: text,
+                      redirectionUrl: body[year].link
+                  };
+                  feeds.push(feed);
+              });
         }
         callback(null, {
             statusCode: 200,
